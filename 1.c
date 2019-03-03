8b33b387db4b977a445b6181e0bc75e3f42e6370
baba <yakoobbaba478@gmail.com>
2019-03-03 21:16:03 +0530

    init

---
+++ 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int max_of_four(int a,int b,int c,int d)
+	{
+	
+	}
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	scanf("%d %d %d %d",&a,&b,&c,&d);
+	printf("largest interger is: %d",max_of_four(a,b,c,d));
+}
+
